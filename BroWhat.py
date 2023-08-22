45adf828a92835d50a9830007eb7cb0951d6137f
pokkeen <135404378+pokkeen@users.noreply.github.com>
2023-08-22 12:47:32 -0500

    Started thign

--- BroWhat.py
+++ BroWhat.py
@@ -1,13 +1,16 @@
-#Yippee
-ResponseList = {
-   
-    "Nice": ["Thank you"],
-    "Mean": ["You suck"]
+import discord
+from discord.ext import commands
 
-}
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix= '!',intents=intents)
 
-input = input()
-if input == "y":
-    print(ResponseList["Nice"][0])
-else:
-    print(ResponseList["Mean"][0])
+@client.event
+async def on_ready():
+    print("Bot is ready!")
+    print("-------------")
+
+@client.command()
+async def test(ctx):
+    return await ctx.send(ctx, "hi")
+
+client.run('MTEzMjgzNjA4MDI5NTYxMjQ2Ng.Gvpt7j.6WuTOQZQDu9aDOea9LZYfXUnSq5ULzhAz1K638')
