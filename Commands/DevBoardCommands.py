9d3cd7a102567eca951af0c97dc2d99171658989
pokkeen <jfcas09@gmail.com>
2023-09-22 15:51:40 -0500

    Testing ground updated

--- Commands/DevBoardCommands.py
+++ Commands/DevBoardCommands.py
@@ -1,22 +1,17 @@
 import TrelloWrapperButClasses
 import datetime
-import roblox
 import requests
 import discord
-from TrelloWrapperButClasses import ListsDict
-from TrelloWrapperButClasses import CardsDict
-from TrelloWrapperButClasses import LabelsDict
+from roblox import Client
 from discord.ext import commands
 from discord import app_commands
 
 
 TrelloBoard = TrelloWrapperButClasses.TrelloBoard
 date = datetime
-RoClient = roblox.client    
+RoClient = Client()
 
 ToDoBoard = TrelloBoard("Dev ToDo Board", '64f8ad85266330f160f747d7')
-
-ToDoBoard.InitalizeBoard()
 
 class DevCommands(commands.Cog):
 
@@ -28,25 +23,34 @@ class DevCommands(commands.Cog):
     @app_commands.describe(tododesc='Put a description of the problem')
     async def AddToDo(self, interaction:discord.Interaction, todoname:str, tododesc:str):
         response = requests.get(f'https://api.blox.link/v4/public/guilds/964609612496142416/discord-to-roblox/441389433061638155',  headers={"Authorization" : 'a91fa14d-c868-48b9-af94-73b60a08561f'})
-        print(response.json())
-        #Put roclient here
-        name = f'{todoname} | created by {UserName}'
-        desc = f'{tododesc} | created by {Put} on {date.date.today()}'
-        ListsDict['Test2'].CreateCard(name,desc)
+        ID = response.json()['robloxID']
+        User =  f'https://www.roblox.com/users/{ID}/profile'
+        UserName = await RoClient.get_user(ID)
+        name = f'{todoname} | created by {UserName.name}'
+        desc = f'{tododesc} | created by {User} on {date.date.today()}'
+        ToDoBoard.CreateCard(name,desc,'To do')
         await interaction.response.send_message(content=f'Created card {todoname}')
     
-    @AddToDo.error
-    async def AddToDoError():
-        return
-    
+
+
     @app_commands.command(name='claimtodo', description='Claims a to do on the trello')
     @app_commands.describe(cardname='Claim a card!')
     async def ClaimToDo(self, interaction:discord.Interaction, cardname:str):
-        return
+        if ToDoBoard.GetBoardCard(cardname)['name'] not in ToDoBoard.GetListCards('To do'):
+            await interaction.response.send_message(content='That card is not in the todo list')
+        response = requests.get(f'https://api.blox.link/v4/public/guilds/964609612496142416/discord-to-roblox/441389433061638155',  headers={"Authorization" : 'a91fa14d-c868-48b9-af94-73b60a08561f'})
+        ID = response.json()['robloxID']
+        User =  f'https://www.roblox.com/users/{ID}/profile'
+        UserName = await RoClient.get_user(ID)
+        OldDesc = ToDoBoard.GetBoardCard(cardname)['desc']
+        NewDesc = f'{OldDesc} \nclaimed by {User}'
+        ToDoBoard.EditCardDesc(NewDesc, cardname)
+        
     
     @app_commands.command(name='displaytodo', description='Displays the to do list')
     async def DisplayToDo(self, interaction:discord.Interaction):
-        
+        return
+         
 
 
 async def setup(client):
--- TrelloWrapperButClasses.py
+++ TrelloWrapperButClasses.py
@@ -5,122 +5,81 @@ AuthDict = {
             'token' : 'ATTA38d848682569db14b6bb4c05aa318b5f16ab9853736bc8cdb24a4630faf55f43428EB0F2'
 }
 
-CardsDict = {}
-ListsDict = {}
-LabelsDict = {}
+class TrelloBoard:
+    def __init__(self, ObjectName, ObjectID):
+        self.ID = ObjectID
+        self.Name = ObjectName
 
+# DO NOT DELETE IMPORTANT FUNCTION IF DELETED REST OF CLASS BREAKS 
+    def GetBoardCard(self, CardName):
+        Response = requests.request('GET',url=f'https://api.trello.com/1/boards/{self.ID}/cards',params=AuthDict)
+        print(Response)
+        for Card in Response.json():
+            if Card['name'].split(' | ')[0] == CardName:
+                return Card
+        print('Card does not exist')
 
+# DO NOT DELETE IMPORTANT FUNCTION IF DELETED REST OF CLASS BREAKS
+    def GetBoardList(self, ListName):
+        Response = requests.request('GET',url=f'https://api.trello.com/1/boards/{self.ID}/lists',params=AuthDict)
+        print(Response)
+        for List in Response.json():
+            if List['name'] == ListName:
+                return List
+        print('List does not exist')
 
-class TrelloObject:
-    def __init__(self, ObjectID, ObjectName):
-        self.ObjectID = ObjectID
-        self.ObjectName = ObjectName
-
-#When Creating a board, you should probably initialize the entire board so what does this mean?
-#When the board is initialized, it gets creates all the cards and list on a specific board
-#This is so I can initialize multiple boards in different parts of a code so its easier to manage certain parts
-class TrelloBoard(TrelloObject):
-    def __init__(self, Name, ID):
-        super().__init__(ID, Name)
+# DO NOT DELETE IMPORTANT FUNCTION IF DELETED REST OF CLASS BREAKS
+    def GetBoardLabel(self, LabelName):
+        Response = requests.request('GET',url=f'https://api.trello.com/1/boards/{self.ID}/labels',params=AuthDict)
+        print(Response)
+        for Label in Response.json():
+            if Label['name'] == LabelName:
+                return Label
+        print('Label does not exist')
     
-    def InitalizeBoard(self):
-        ListsFromBoard = requests.request('GET', url=f'https://api.trello.com/1/boards/{self.ObjectID}/lists', params=AuthDict)
-        for Lists in ListsFromBoard.json():
-            ListsDict[Lists['name']] = TrelloList(Lists['name'],Lists['id'])
-        CardsFromBoard = requests.request('GET', url=f'https://api.trello.com/1/boards/{self.ObjectID}/cards', params=AuthDict)
-        for Cards in CardsFromBoard.json():
-            CardsDict[Cards['name']] = TrelloCard(Cards['name'],Cards['id'])
-        LabelsFromBoard = requests.request('GET', url=f'https://api.trello.com/1/boards/{self.ObjectID}/labels', params=AuthDict)
-        for Labels in LabelsFromBoard.json():
-            LabelsDict[Labels['name']] = TrelloLabel(Labels['name'],Labels['id'])
-
-     
-class TrelloList(TrelloObject):
-    def __init__(self, Name, ID):
-        super().__init__(ID, Name) 
-    
-    def CreateCard(self, Name=None, Desc=None):
-        AuthDict['idList'] = self.ObjectID
+    def CreateCard(self, Name=None, Desc=None, List=None):
+        AuthDict['idList'] = self.GetBoardList(List)
         AuthDict['name'] = Name
         AuthDict['desc'] = Desc
-        Response = requests.request(
-            "POST",
-            url=f'https://api.trello.com/1/cards',
-            params=AuthDict
-            )
+        Response = requests.request("POST",url=f'https://api.trello.com/1/cards',params=AuthDict)
         print(Response)
-
-    def GetListCards(self):
+    
+    def GetListCards(self, List=None):
+        ID = self.GetBoardList(List)['id']
         CardList = []
-        Response = requests.request(
-            'GET',
-            url=f'https://api.trello.com/1/lists/{self.ObjectID}/cards',
-            params=AuthDict
-        )
+        Response = requests.request('GET',url=f'https://api.trello.com/1/lists/{ID}/cards',params=AuthDict)
         for cards in Response.json():
             CardList.append(cards['name'])
+        print(Response)
         return CardList
-
     
-class TrelloCard(TrelloObject):
-    def __init__(self, Name, ID):
-        super().__init__(ID, Name)
-    
-
-    def GetCard(self):
-        Response = requests.request(
-            "GET", 
-            url=f'https://api.trello.com/1/cards/{self.ObjectID}',
-            params=AuthDict
-        )
-        print(Response.json())
-
-    def EditName(self,Name):
+    def EditCardName(self,Name=None,Card=None):
+        ID = self.GetBoardCard(Card)['id']
         AuthDict['name'] = Name
-        Response = requests.request(
-            "PUT",
-            url=f'https://api.trello.com/1/cards/{self.ObjectID}',
-            params=AuthDict
-        )
-        print(Response)
-    
-    def EditDesc(self,Desc):
-        AuthDict['desc'] = Desc
-        Response = requests.request(
-            "PUT",
-            url=f'https://api.trello.com/1/cards/{self.ObjectID}',
-            params=AuthDict
-        )
-        print(Response)
-    
-    def AddLabel(self,Label):
-        AuthDict['idLabels'] = Label.ObjectID
-        Response = requests.request(
-            "PUT",
-            url=f'https://api.trello.com/1/cards/{self.ObjectID}',
-            params=AuthDict
-        )
-        print(Response)
-    
-    def RemoveLabel(self,Label):
-        Response = requests.request(
-            "DELETE",
-            url=f'https://api.trello.com/1/cards/{self.ObjectID}/idLabels/{Label.ObjectID}',
-            params=AuthDict
-        )
+        Response = requests.request("PUT",url=f'https://api.trello.com/1/cards/{ID}',params=AuthDict)
         print(Response)
 
-    def Move(self,ListID):
-        AuthDict['idList'] = ListID
-        Response = requests.request(
-            "PUT",
-            url=f'https://api.trello.com/1/cards/{self.ObjectID}',
-            params=AuthDict
-        )
+    def EditCardDesc(self,Desc=None,Card=None):
+        ID = self.GetBoardCard(Card)['id']
+        AuthDict['desc'] = Desc
+        Response = requests.request("PUT",url=f'https://api.trello.com/1/cards/{ID}',params=AuthDict)
+        print(Response)
+
+    def AddLabel(self, Card=None, Label=None):
+        ID = self.GetBoardCard(Card)['id']
+        AuthDict['idLabels'] = self.GetBoardLabel(Label)['id']
+        Response = requests.request("PUT",url=f'https://api.trello.com/1/cards/{ID}',params=AuthDict)
+        print(Response)
+    
+    def RemoveLabel(self,Card=None, Label=None):
+        ID1 = self.GetBoardCard(Card)
+        ID2 = self.GetBoardLabel(Label)
+        Response = requests.request("DELETE",url=f'https://api.trello.com/1/cards/{ID1}/idLabels/{ID2}',params=AuthDict)
+        print(Response)
+    
+    def Move(self,Card=None, List=None):
+        ID = self.GetBoardCard(Card)
+        AuthDict['idList'] = self.GetBoardList(List)
+        Response = requests.request("PUT",url=f'https://api.trello.com/1/cards/{ID}',params=AuthDict)
         print(Response)
         print(Response.json())
-
-class TrelloLabel(TrelloObject):
-    def __init__(self, Name, ID):
-        super().__init__(ID, Name)
-
