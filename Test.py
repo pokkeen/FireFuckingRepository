09895a5723d72b97ba4e5aec554a481a37222079
pokkeen <jfcas09@gmail.com>
2023-09-22 15:51:50 -0500

    fuck

--- Test.py
+++ Test.py
@@ -1,14 +1,10 @@
 import TrelloWrapperButClasses
-from TrelloWrapperButClasses import ListsDict
-from TrelloWrapperButClasses import CardsDict
-from TrelloWrapperButClasses import LabelsDict
-
-import requests
 
 
 TrelloBoard = TrelloWrapperButClasses.TrelloBoard
 
 TestBoard = TrelloBoard("Dev ToDo Board", '64f8ad85266330f160f747d7')
 
-TestBoard.InitalizeBoard()
+TestBoard.EditCardDesc('What','Card4')
+
 
