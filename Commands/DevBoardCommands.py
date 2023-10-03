c7e2300dee56986584fd4719108493491eb0082e
pokkeen <jfcas09@gmail.com>
2023-10-03 09:39:25 -0500

    Changed Name

--- Commands/DevBoardCommands.py
+++ Commands/DevBoardCommands.py
@@ -16,6 +16,9 @@ RoClient = Client()
 
 ToDoBoard = TrelloBoard("Dev ToDo Board", '64f8ad85266330f160f747d7')
 
+def ErrorHandler():
+    pass
+
 class DevCommands(commands.Cog):
 
     def __init__(self, client: commands.Bot):
@@ -23,9 +26,11 @@ class DevCommands(commands.Cog):
 
     @app_commands.command(name='addtodo', description='Adds to the to do list on the trello')
     @app_commands.describe(todoname='Select a name for the problem', tododesc='Put a description of the problem')
-    @app_commands.guild_only()
-    @app_commands.check(is_in_guild)
     async def AddToDo(self, interaction:discord.Interaction, todoname:str, tododesc:str):
+        if tododesc == None or todoname == None:
+            Error = ErrorHandler()
+            await interaction.response.send_message(content=Error)
+        
         response = requests.get(f'https://api.blox.link/v4/public/guilds/964609612496142416/discord-to-roblox/441389433061638155',  headers={"Authorization" : 'a91fa14d-c868-48b9-af94-73b60a08561f'})
         ID = response.json()['robloxID']
         User =  f'https://www.roblox.com/users/{ID}/profile'
@@ -34,15 +39,9 @@ class DevCommands(commands.Cog):
         desc = f'{tododesc} | created by {User} on {date.date.today()}'
         ToDoBoard.CreateCard(name,desc,'To do')
         await interaction.response.send_message(content=f'Created card {todoname}')
-    
-    @AddToDo.error
-    async def ToDoError(self, interaction:discord.Interaction, error):
-        await interaction.response.send_message(content=f'Wrong server')
 
     @app_commands.command(name='claimtodo', description='Claims a to do on the trello')
     @app_commands.describe(cardname='Claim a card!')
-    @app_commands.guild_only()
-    @app_commands.check(is_in_guild)
     async def ClaimToDo(self, interaction:discord.Interaction, cardname:str):
         if ToDoBoard.GetBoardCard(cardname)['name'] not in ToDoBoard.GetListCards('To do'):
             await interaction.response.send_message(content='That card is not in the todo list')
@@ -55,22 +54,12 @@ class DevCommands(commands.Cog):
         ToDoBoard.EditCardDesc(NewDesc, cardname)
         await interaction.response.send_message(content=f'Card Claimed!')
 
-    @ClaimToDo.error
-    async def ToDoError(self, interaction:discord.Interaction, error):
-        await interaction.response.send_message(content=f'Wrong server') 
-    
     @app_commands.command(name='displaytodo', description='Displays the to do list')
-    @app_commands.guild_only()
-    @app_commands.check(is_in_guild)
     async def DisplayToDo(self, interaction:discord.Interaction):
         ebed = discord.Embed(title='To do list for devs', description='The to do list')
         for tasks in ToDoBoard.GetListCards('To do'):
             ebed.add_field(name=tasks['name'], value=tasks['desc'], inline=False)
         await interaction.response.send_message(embed=ebed)
-
-    @DisplayToDo.error
-    async def ToDoError(self, interaction:discord.Interaction, error):
-        await interaction.response.send_message(content=f'Wrong server') 
         
 
 
