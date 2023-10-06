45ac3820e2368b89124113f3b72577b9ecf05cbc
pokkeen <jfcas09@gmail.com>
2023-10-06 00:18:44 -0500

    teted

--- Test.py
+++ Test.py
@@ -1,10 +1,9 @@
-import TrelloWrapperButClasses
+import TrelloWrapper
 
 
-TrelloBoard = TrelloWrapperButClasses.TrelloBoard
+TrelloBoard = TrelloWrapper.TrelloBoard
 
 TestBoard = TrelloBoard("Dev ToDo Board", '64f8ad85266330f160f747d7')
 
-TestBoard.EditCardDesc('What','Card4')
-
-
+for tasks in TestBoard.GetListCards('To do'):
+    tasks['labels'][0]['name']
